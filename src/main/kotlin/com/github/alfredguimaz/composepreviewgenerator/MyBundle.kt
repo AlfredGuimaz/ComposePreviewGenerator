08bc218fddaee67730a26e262547d748781e1865
GitHub Action <action@github.com>
2023-12-01 03:23:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/alfredguimaz/composepreviewgenerator/MyBundle.kt
+++ src/main/kotlin/com/github/alfredguimaz/composepreviewgenerator/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.alfredguimaz.composepreviewgenerator
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/alfredguimaz/composepreviewgenerator/services/MyProjectService.kt
+++ src/main/kotlin/com/github/alfredguimaz/composepreviewgenerator/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.alfredguimaz.composepreviewgenerator.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.alfredguimaz.composepreviewgenerator.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
